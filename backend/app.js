323e48155ff3869dee892edc2d28193bf1483545
Ilya Mischenko <timur.demichev@yandex.ru>
2022-01-16 15:13:33 +0200

    15 review1: fix cors2

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
-// const cors = require('./middlewares/cors');
-const cors = require('cors');
 const NotFoundError = require('./errors/NotFoundError'); // 404
 const { validateSignUp, validateSignIn } = require('./middlewares/validators');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-require('dotenv').config();
-
-const corsOptions = {
-  origin: [
-    'https://api.mesto.demichev.nomoredomains.rocks',
-    'http://api.mesto.demichev.nomoredomains.rocks',
-    'https://mesto.demichev.nomoredomains.rocks',
-    'http://mesto.demichev.nomoredomains.rocks',
-    'http://localhost:3000',
-  ],
-  credentials: true,
-};
+app.use(cors);
 
 app.use(cookieParser());
 
@@ -42,8 +32,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {});
 app.use(express.json());
 
 app.use(requestLogger);
-
-app.use(cors(corsOptions));
 
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -80,5 +68,91 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
+
+// const express = require('express');
+// const cookieParser = require('cookie-parser');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const helmet = require('helmet');
+// const { errors } = require('celebrate');
+// // const cors = require('./middlewares/cors');
+// const cors = require('cors');
+// const NotFoundError = require('./errors/NotFoundError'); // 404
+// const { validateSignUp, validateSignIn } = require('./middlewares/validators');
+// const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+// const { PORT = 3000 } = process.env;
+// const app = express();
+
+// require('dotenv').config();
+
+// const corsOptions = {
+//   origin: [
+//     'https://api.mesto.demichev.nomoredomains.rocks',
+//     'http://api.mesto.demichev.nomoredomains.rocks',
+//     'https://mesto.demichev.nomoredomains.rocks',
+//     'http://mesto.demichev.nomoredomains.rocks',
+//     'http://localhost:3000',
+//   ],
+//   credentials: true,
+// };
+
+// app.use(cookieParser());
+
+// app.use(helmet());
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// const { createUser, login } = require('./controllers/users');
+
+// const auth = require('./middlewares/auth');
+
+// mongoose.connect('mongodb://localhost:27017/mestodb', {});
+
+// app.use(express.json());
+
+// app.use(requestLogger);
+
+// app.use(cors(corsOptions));
+
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
+
+// app.post('/signin', validateSignIn, login);
+// app.post('/signup', validateSignUp, createUser);
+
+// app.use(auth);
+
+// app.use('/users', require('./routes/users'));
+// app.use('/cards', require('./routes/cards'));
+
+// app.use('*', () => {
+//   throw new NotFoundError('Запрашиваемый ресурс не найден');
+// });
+
+// app.use(errorLogger);
+
+// app.use(errors());
+
+// app.use((err, req, res, next) => {
+//   const { statusCode = 500, message } = err;
+//   res
+//     .status(statusCode)
+//     .send({
+//       message: statusCode === 500
+//         ? 'На сервере произошла ошибка'
+//         : message,
+//     });
+//   next();
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}`);
+// });
