c9e780a6fbd01daf09603e68b2552fedfc3d8840
Ilya Mischenko <timur.demichev@yandex.ru>
2022-02-01 19:51:49 +0200

    15 review1: cors4

--- backend/app.js
+++ backend/app.js
@@ -12,25 +12,27 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-const allowedCors = [
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'https://mesto.demichev.nomoredomains.rocks',
-  'http://mesto.demichev.nomoredomains.rocks',
-];
+app.use(cors);
 
-app.use(
-  cors({
-    credentials: true,
-    origin(origin, callback) {
-      if (allowedCors.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  }),
-);
+// const allowedCors = [
+//   'http://localhost:3000',
+//   'https://localhost:3000',
+//   'https://mesto.demichev.nomoredomains.rocks',
+//   'http://mesto.demichev.nomoredomains.rocks',
+// ];
+
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin(origin, callback) {
+//       if (allowedCors.includes(origin) || !origin) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//   }),
+// );
 
 app.use(cookieParser());
 
